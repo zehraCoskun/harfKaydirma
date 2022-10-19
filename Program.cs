712324e64a749b3f8d315be5d5267267e7e3e0d7
xzehrax <rabiazehraunal@hotmail.com>
2022-10-19 13:27:30 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        basla:
+        string input = "";
+        Console.WriteLine("kelime giriniz");
+        input = Console.ReadLine();
+        
+
+        for (int i = 1; i < input.Length; i++)
+        {
+            Console.Write(input[i]);
+        }
+        Console.Write(input[0]);
+        Console.WriteLine("");
+        goto basla;
+    }
+}
